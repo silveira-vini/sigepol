f5642c05b2e92aaee815e0e71adb6e7ee203169c
silveira-vini <vsribeiro87@gmail.com>
2024-12-10 12:02:50 -0300

    Initial commit

---
+++ src/main/java/silveira/vinicius/sigepol/SigepolApplication.java
@@ -0,0 +1,13 @@
+package silveira.vinicius.sigepol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigepolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigepolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silveira/vinicius/sigepol/SigepolApplicationTests.java
@@ -0,0 +1,13 @@
+package silveira.vinicius.sigepol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigepolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
