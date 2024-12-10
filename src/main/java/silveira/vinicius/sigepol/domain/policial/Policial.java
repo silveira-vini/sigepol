daeccd8cda0c75a074d26b3200de22020933e06e
silveira-vini <vsribeiro87@gmail.com>
2024-12-10 15:54:36 -0300

    entity Policial and migration commit

--- src/main/java/silveira/vinicius/sigepol/domain/policial/Policial.java
+++ src/main/java/silveira/vinicius/sigepol/domain/policial/Policial.java
@@ -1,13 +1,24 @@
 package silveira.vinicius.sigepol.domain.policial;
 
+import jakarta.persistence.*;
+import lombok.*;
 import silveira.vinicius.sigepol.domain.dadosFuncionais.DadosFuncionais;
 import silveira.vinicius.sigepol.domain.endereco.Endereco;
 import silveira.vinicius.sigepol.domain.formacao.Formacao;
 
 import java.util.Date;
 
+@Table(name = "policial")
+@Entity(name = "Policial")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Policial {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
@@ -15,8 +26,11 @@ public class Policial {
     private Date nascimento;
     private String nomeMae;
     private String nomePai;
+    @Embedded
     private Endereco endereco;
+    @Embedded
     private DadosFuncionais dadosFuncionais;
+    @Embedded
     private Formacao formacao;
     private boolean ativo;
 
